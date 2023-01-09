de7fba899df04b6d653c9f77bf2d56e0cd5b2f00
Nicoleta Serban <nicoleta.ban@gmail.com>
2023-01-09 12:50:52 +0200

    filter works also with just one input choose

--- controllers/filter.js
+++ controllers/filter.js
@@ -15,3 +15,37 @@ module.exports = {
     res.render("home.ejs", { feed: feed, user: req.user });
   },
 };
+module.exports = {
+  getFilter: async (req, res) => {
+    console.log(req.body);
+
+    // Create an empty filter object
+    const filter = {};
+
+    // Check if the request body has a 'type' field, and add it to the filter object if it does
+    if (req.body.type) {
+      filter.type = req.body.type;
+    }
+
+    // Check if the request body has a 'forAdoption' field, and add it to the filter object if it does
+    if (req.body.forAdoption) {
+      filter.forAdoption = req.body.forAdoption;
+    }
+
+    // Check if the request body has a 'forFoster' field, and add it to the filter object if it does
+    if (req.body.forFoster) {
+      filter.forFoster = req.body.forFoster;
+    }
+
+    // Check if the request body has a 'location' field, and add it to the filter object if it does
+    if (req.body.location) {
+      filter.location = req.body.location;
+    }
+
+    // Query the Animal collection with the filter object
+    const feeds = await Animal.find(filter).sort({ postDate: "desc" }).lean();
+
+    // Render the home view template with the filtered results and the user object
+    res.render("home.ejs", { feeds: feeds, user: req.user });
+  },
+};
